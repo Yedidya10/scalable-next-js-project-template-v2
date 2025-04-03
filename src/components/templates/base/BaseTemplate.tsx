c1a38dd470d43a9eec50836e877cd1b41351d1e9
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-04-03 09:52:09 +0300

    feat: add BaseTemplate component with TypeScript interface and Storybook stories

---
+++ src/components/templates/base/BaseTemplate.tsx
@@ -0,0 +1,34 @@
+export interface IBaseTemplate {
+  sampleTextProp: string
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string
+  /**
+   * How large should the BaseTemplate be?
+   */
+  size?: 'small' | 'medium' | 'large'
+  /**
+   * BaseTemplate contents
+   */
+  label: string
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void
+}
+
+const BaseTemplate: React.FC<IBaseTemplate> = ({
+  primary = false,
+  label,
+  sampleTextProp,
+  ...props
+}) => {
+  return <div>{sampleTextProp}</div>
+}
+
+export default BaseTemplate
